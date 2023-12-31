7829d222305420333834c11844fd8cd2344c750e
Gia Hau <nguyengiahau2004@gmail.com>
2023-12-31 22:50:33 +0700

    31-12-2023

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,3 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
     loadHTML('../includes/header.html', 'header');
     loadHTML('../includes/footer.html', 'footer');
 });
+
+function showMenu() {
+    var e = document.getElementById('menu');
+    if (e.style.right == '-100%') {
+        e.style.right = '0%';
+        document.getElementById('icon-bar').innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    }
+    else {
+        e.style.right = '-100%';
+        document.getElementById('icon-bar').innerHTML = '<i class="fa-solid fa-bars"></i>';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('content-2').style.height = document.getElementById('content-1').offsetHeight + 'px';
+});
