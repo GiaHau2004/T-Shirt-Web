6c0f8f3ad2baf62bdccc981285c720606f15db7a
Gia Hau <nguyengiahau2004@gmail.com>
2024-01-01 20:24:30 +0700

    1-1-2024

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,7 +22,39 @@ function showMenu() {
         document.getElementById('icon-bar').innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 }
+function animateElements(index) {
+    var elements = document.querySelectorAll(".banner");
+
+    setTimeout(function () {
+        elements[index].classList.remove("slide-remove");
+        elements[index].classList.add("slide-active");
+
+        if (index > 0) {
+            elements[index - 1].classList.add("slide-remove");
+            elements[index - 1].classList.remove("slide-active");
+        } else {
+            elements[elements.length - 1].classList.add("slide-remove");
+            elements[elements.length - 1].classList.remove("slide-active");
+        }
+
+        if (index === elements.length - 1) {
+            // Khi đến phần tử cuối cùng, gọi lại hàm với index bằng 0 để lặp lại từ đầu
+            animateElements(0);
+        } else {
+            // Ngược lại, gọi lại hàm với index tăng lên 1 để di chuyển đến phần tử tiếp theo
+            animateElements(index + 1);
+        }
+    }, 2000 * index);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    animateElements(0);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('content-2').style.height = document.getElementById('content-1').offsetHeight + 'px';
+    document.querySelectorAll(".banner").forEach(function (e) {
+        e.classList.add("slide-remove");
+    });
+
 });
